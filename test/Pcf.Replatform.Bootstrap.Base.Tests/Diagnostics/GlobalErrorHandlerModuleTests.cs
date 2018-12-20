3cac720efe7fdb1cad2a6f6c1a31004452f26d53
Alfus Inigo Jaganathan <alfusinigo.j@gmail.com>
2018-12-20 13:34:53 -0500

    intermediate checkin

---
+++ test/Pcf.Replatform.Bootstrap.Base.Tests/Diagnostics/GlobalErrorHandlerModuleTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Pivotal.CloudFoundry.Replatform.Bootstrap.Base.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Pivotal.CloudFoundry.Replatform.Bootstrap.Base.Tests.Diagnostics
+{
+    [TestClass]
+    public class GlobalErrorHandlerModuleTests
+    {
+        Mock<ILoggerFactory> loggerFactory;
+        Mock<IHost> host;
+
+        public GlobalErrorHandlerModuleTests()
+        {
+            loggerFactory = new Mock<ILoggerFactory>();
+            loggerFactory.Setup(l => l.CreateLogger(nameof(GlobalErrorHandlerModule))).Returns(new Mock<ILogger<GlobalErrorHandlerModule>>().Object);
+
+            host = new Mock<IHost>();
+            var services = new ServiceCollection();
+            host.SetupGet(h => h.Services).Returns(services.BuildServiceProvider());
+        }
+
+        [TestMethod]
+        public void Test_IsTypeOfHttpModule()
+        {
+            Assert.IsTrue(new GlobalErrorHandlerModule() is IHttpModule);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_ThrowsExceptionIfLoggerFactoryIsNull()
+        {
+            Assert.IsTrue(new GlobalErrorHandlerModule() is IHttpModule);
+        }
+    }
+}
---
+++ test/Pcf.Replatform.Bootstrap.Base.Tests/TestHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pivotal.CloudFoundry.Replatform.Bootstrap.Base.Tests
+{
+    public static class TestHelper
+    {
+        public static TReturn InvokeNonPublicInstanceMethod<TReturn>(this object parentObject, string methodName, params object[] methodParameters)
+        {
+            return (TReturn)InvokeNonPublicInstanceMethod<TReturn>(parentObject, methodName, methodParameters);
+        }
+
+        public static object InvokeNonPublicInstanceMethod(this object parentObject, string methodName, params object[] methodParameters)
+        {
+            var method = parentObject.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (method == null)
+                throw new MissingMethodException(parentObject.GetType().FullName, methodName);
+
+            try
+            {
+                return method.Invoke(parentObject, methodParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex.InnerException ?? ex;
+            }
+        }
+
+        public static object GetNonPublicStaticFieldValue(Type parentType, string fieldName)
+        {
+            var field = parentType.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (field == null)
+                throw new MissingMemberException(parentType.FullName, fieldName);
+
+            return field.GetValue(null);
+        }
+
+        public static void SetNonPublicStaticFieldValue(Type parentType, string fieldName, object value)
+        {
+            var field = parentType.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (field == null)
+                throw new MissingMemberException(parentType.FullName, fieldName);
+
+            field.SetValue(null, value);
+        }
+
+        public static TReturn GetNonPublicInstanceFieldValue<TReturn>(this object parentObject, string fieldName)
+        {
+            var field = parentObject.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (field == null)
+                throw new MissingMemberException(parentObject.GetType().FullName, fieldName);
+
+            return (TReturn)field.GetValue(parentObject);
+        }
+
+        public static object GetNonPublicInstanceFieldValue(this object parentObject, string fieldName)
+        {
+            var field = parentObject.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (field == null)
+                throw new MissingMemberException(parentObject.GetType().FullName, fieldName);
+
+            return field.GetValue(parentObject);
+        }
+
+        public static void SetNonPublicInstanceFieldValue(this object parentObject, string fieldName, object value)
+        {
+            var field = parentObject.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+
+            if (field == null)
+                throw new MissingMemberException(parentObject.GetType().FullName, fieldName);
+
+            field.SetValue(parentObject, value);
+        }
+    }
+}
