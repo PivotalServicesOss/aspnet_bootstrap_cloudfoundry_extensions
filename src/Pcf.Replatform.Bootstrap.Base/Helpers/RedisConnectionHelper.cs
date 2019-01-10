ae9ee4ae0dcdc047d5adb896c00a4454a495824d
Alfus Inigo Jaganathan <alfusinigo.j@gmail.com>
2019-01-10 15:19:03 -0500

    added redis settings and web.config.transform

---
+++ src/Pcf.Replatform.Bootstrap.Base/Helpers/RedisConnectionHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.Redis;
+using StackExchange.Redis;
+using System;
+
+namespace Pivotal.CloudFoundry.Replatform.Bootstrap.Base.Helpers
+{
+    public static class RedisConnectionHelper
+    {
+        static IConnectionMultiplexer connectionMultiplexer;
+        static RedisSessionStateProvider redisSessionStateProvider;
+
+        static RedisConnectionHelper()
+        {
+            connectionMultiplexer = AppConfig.GetService<IConnectionMultiplexer>() ?? throw new ArgumentNullException(nameof(IConnectionMultiplexer));
+        }
+
+        public static string GetConnectionString()
+        {
+            return connectionMultiplexer.Configuration;
+        }
+    }
+}
