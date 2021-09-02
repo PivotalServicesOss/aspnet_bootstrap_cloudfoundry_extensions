5ebc1e639e6b1fcdee53eb146429e55df2e7797a
Alfus Inigo Jaganathan <alfusinigo.j@gmail.com>
2021-09-02 01:06:26 -0400

    Samples updated and tested

---
+++ samples/All.In.One.WebForms1/Health/MyCustomHealthContributor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Steeltoe.Common.HealthChecks;
+
+namespace All.In.One.WebForms.Health
+{
+    public class MyCustomHealthContributor : IHealthContributor
+    {
+        public string Id => "MyCustomHealth";
+
+        public HealthCheckResult Health()
+        {
+            return new HealthCheckResult() { Status = HealthStatus.UP };
+        }
+    }
+}
---
+++ samples/All.In.One1/App_Start/WebApiConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.WebHost;
+
+namespace All.In.One
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var httpControllerRouteHandler = typeof(HttpControllerRouteHandler).GetField("_instance",
+                System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+
+            if (httpControllerRouteHandler != null)
+            {
+                httpControllerRouteHandler.SetValue(null,
+                    new Lazy<HttpControllerRouteHandler>(() => new SessionHttpControllerRouteHandler(), true));
+            }
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
